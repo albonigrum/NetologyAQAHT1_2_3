1779b0a3c68d4d690ea7026a5a6b56004927163a
Ivan Grebenschikov <albonigrum@gmail.com>
2020-07-29 21:50:51 +0300

    Add Gradle, add PostmanEchoTest with incorrect test.

---
+++ src/test/java/ru/netology/PostmanEcho/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.PostmanEcho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет это тестовые данные!!")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет это тестовые данные!"))
+        ;
+    }
+}
